1d7c27ab69578981a02d2fb23d2ee4fd6ae9b078
mattieuf <ffmattieu@gmail.com>
2023-07-28 15:11:00 +0200

    Config OK

---
+++ src/main/java/fr/plage/configuration/MaConfigurationPlage.java
@@ -0,0 +1,10 @@
+package fr.plage.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("fr.plage.Services")
+public class MaConfigurationPlage {
+
+}
