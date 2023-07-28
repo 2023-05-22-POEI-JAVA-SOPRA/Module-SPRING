8e4da54ed9b2b7760dc75c1556b374a96e310745
mattieuf <ffmattieu@gmail.com>
2023-07-28 16:18:26 +0200

    add demandeReservation et plus nouvelle methode

--- src/main/java/fr/plage/dao/ReservationDao.java
+++ src/main/java/fr/plage/dao/ReservationDao.java
@@ -2,6 +2,7 @@ package fr.plage.dao;
 
 import org.springframework.stereotype.Component;
 
+import fr.plage.entity.DemandeReservation;
 import fr.plage.entity.Reservation;
 
 @Component
@@ -11,6 +12,23 @@ public class ReservationDao {
 		Reservation savedRes = new Reservation(reservation);
 		System.out.println("Une réservation a été créée : " + savedRes.toString());
 		return savedRes;
+	}
+	
+	public Reservation saveFromDemande(DemandeReservation demRes) {
+		Reservation savedRes = new Reservation();
+
+		if (demRes.getHotel().toLowerCase().startsWith("super")) {
+			return null;
+		}
+		else if (!(demRes.getPlage().toLowerCase().endsWith("plage"))) {
+			return null;
+		}
+		else {
+			savedRes.setNom(demRes.getNom());
+			savedRes.setDebut(demRes.getDebut());
+			savedRes.setFin(demRes.getFin());
+			return savedRes;
+		}
 	}
 	
 	public String getById(Integer id) {
