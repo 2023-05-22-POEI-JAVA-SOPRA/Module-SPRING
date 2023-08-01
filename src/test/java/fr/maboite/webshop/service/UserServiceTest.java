04f57abe9cbf8b008da2dbea47fe5ead2104c681
Nicolas Postaire <postairenicolas2501@gmail.com>
2023-08-01 17:13:57 +0200

    ajout tests

--- src/test/java/fr/maboite/webshop/service/UserServiceTest.java
+++ src/test/java/fr/maboite/webshop/service/UserServiceTest.java
@@ -1,5 +1,34 @@
 package fr.maboite.webshop.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import fr.maboite.webshop.model.User;
+import fr.maboite.webshop.spring.real.configuration.SpringConfiguration;
+
+@SpringJUnitConfig(SpringConfiguration.class)
 public class UserServiceTest {
+	@Autowired
+	UserService service;
+	
+	@Test
+	void get_by_existing_id() {
+		User resultTested = service.getById(2);
+		User result = new User(2, "paul@paul.com", "motdepasse", 655615546);
+		assertEquals(result, resultTested);
+	}
+	
+	@Test
+	void get_by_non_existing_id() {
+		assertThrows(NoSuchElementException.class,
+				() -> service.getById(1000));
+	}
+	
 
 }
