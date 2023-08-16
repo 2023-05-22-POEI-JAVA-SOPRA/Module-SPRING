d0eeee046a230ba86d17da6462c1f69fc2d58a7e
François Caremoli <francois.caremoli@wanadoo.fr>
2023-08-16 23:44:03 +0200

    Modification Controleur des planètes et de planete-detail pour régler les problèmes de titre

--- src/main/java/fr/maboite/webshop/controller/correction/PlaneteController.java
+++ src/main/java/fr/maboite/webshop/controller/correction/PlaneteController.java
@@ -1,10 +1,41 @@
 package fr.maboite.webshop.controller.correction;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import fr.maboite.webshop.model.Planete;
 
 @Controller
 public class PlaneteController {
 
+	@Autowired
+	private PlaneteService planeteService;
 
+	@RequestMapping("/planete/planete-detail")
+	public ModelAndView affichePlaneteParRequestParam(@RequestParam("id") Integer id) {
+
+		Planete planete = this.planeteService.getById(id);
+
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/planete/planete-detail");
+
+		mav.addObject("planete", planete);
+
+		return mav;
+	}
+
+	@RequestMapping("/planete/planete-detail/{id}")
+	public ModelAndView affichePlaneteParPathVariable(@PathVariable("id") Integer id) {
+		return affichePlaneteParRequestParam(id);
+	}
+
+	@RequestMapping(path = "/planete/planetes-list")
+	public ModelAndView planetes() {
+		return new ModelAndView("/planete/planetes-list", "planetes", this.planeteService.getAll());
+	}
 
 }
