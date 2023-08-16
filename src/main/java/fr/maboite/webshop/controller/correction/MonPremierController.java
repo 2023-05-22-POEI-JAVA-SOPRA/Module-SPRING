39c17dce8397652788813cd730d667f5bc4d2ead
Laurent Plessis <Laurent.plessis@gmail.com>
2023-08-16 14:28:54 +0200

    exercice th:each itérer une liste de camion

--- src/main/java/fr/maboite/webshop/controller/correction/MonPremierController.java
+++ src/main/java/fr/maboite/webshop/controller/correction/MonPremierController.java
@@ -1,5 +1,8 @@
 package fr.maboite.webshop.controller.correction;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +34,21 @@ public class MonPremierController {
 	public String monCamion() {
 		Camion camion = new Camion("Monbo","Camion");
 		return camion.getName();
+	}
+	
+	@ModelAttribute("maListeCamion")
+	public List<Camion> maListeCamion(){
+		
+		List<Camion> camions=new ArrayList<Camion>();
+		Camion a=new Camion("A","1");
+		camions.add(a);
+		Camion b=new Camion("B","2");
+		camions.add(b);
+		Camion c=new Camion("C","3");
+		camions.add(c);
+		Camion d=new Camion("D","4");
+		camions.add(d);
+		return camions;
 	}
 	
 	
