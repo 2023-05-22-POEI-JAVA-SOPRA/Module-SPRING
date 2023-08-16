c46f370dbe43df73056ea10c2fd4a948adaac219
François Caremoli <francois.caremoli@wanadoo.fr>
2023-08-16 13:28:53 +0200

    Utilisation de la classe Camion dans Thymeleaf

---
+++ src/main/java/fr/maboite/webshop/controller/correction/Camion.java
@@ -0,0 +1,25 @@
+package fr.maboite.webshop.controller.correction;
+
+public class Camion {
+
+	private Integer id;
+
+	private String nom;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+}
--- src/main/java/fr/maboite/webshop/controller/correction/MonPremierController.java
+++ src/main/java/fr/maboite/webshop/controller/correction/MonPremierController.java
@@ -1,10 +1,25 @@
 package fr.maboite.webshop.controller.correction;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MonPremierController {
+
+	@ModelAttribute("monMessage")
+	public String monMessage() {
+		return "Message dynamique !";
+	}
+
+	@ModelAttribute("camion")
+	public Camion messageDynamique() {
+		// En fonction d'éléments (du contexte, de la session, d'une base de donnée...)
+		Camion camion = new Camion();
+		camion.setId(1);
+		camion.setNom("Renault Tractor");
+		return camion;
+	}
 
 	@RequestMapping(path = "/salut")
 	public String maPremiereMethodeDeController() {
