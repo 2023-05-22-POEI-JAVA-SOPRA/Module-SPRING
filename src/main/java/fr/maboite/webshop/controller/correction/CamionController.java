277e13db9aa7930b3d024a0da22161f42c7ac84c
François Caremoli <francois.caremoli@wanadoo.fr>
2023-08-17 11:39:02 +0200

    Ajout du formulaire de consultation/modification des camions

--- src/main/java/fr/maboite/webshop/controller/correction/CamionController.java
+++ src/main/java/fr/maboite/webshop/controller/correction/CamionController.java
@@ -1,7 +1,10 @@
 package fr.maboite.webshop.controller.correction;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,7 +12,9 @@ import org.springframework.web.servlet.ModelAndView;
 public class CamionController {
 
 
-	@RequestMapping("/camion")
+	@GetMapping("/camion")
+	// Equivaut à :
+	// @RequestMapping(path = "/camion", method = RequestMethod.GET)
 	public ModelAndView afficheCamionParRequestParam(@RequestParam("id") Integer id) {
 		System.out.println("A partir du paramètre de requête, j'affiche la page pour le camion : " + id);
 
@@ -18,10 +23,26 @@ public class CamionController {
 		camion.setNom("Camion : " + id);
 
 		ModelAndView mav = new ModelAndView();
-		mav.setViewName("camion");
+		mav.setViewName("camion-detail");
 
 		mav.addObject("camion", camion);
 		mav.addObject("autreTruc", "Coucou!");
+
+		return mav;
+	}
+
+	@PostMapping("/camion")
+	public ModelAndView sauvegardeCamion(@ModelAttribute("camion") Camion camion,
+			BindingResult bindingResult) {
+
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("camion-detail");
+		mav.addObject("camion", camion);
+
+		if (bindingResult.hasErrors()) {
+			System.out.println("Erreur de camion, je ne sauvegarde pas.");
+			mav.addObject("errorString", "Erreur dans le camion!!!");
+		}
 
 		return mav;
 	}
